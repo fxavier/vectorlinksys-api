b75ab38bf37aa4b1ee9061ee5d07000bcb859eaa
Xavier Francisco <xavierfrancisco353@gmail.com>
2018-03-03 13:47:19 +0200

    Initial commit

---
+++ src/main/java/com/xavier/vectorlink/vectorlinksysapi/VectorlinksysApiApplication.java
@@ -0,0 +1,12 @@
+package com.xavier.vectorlink.vectorlinksysapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorlinksysApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectorlinksysApiApplication.class, args);
+	}
+}
